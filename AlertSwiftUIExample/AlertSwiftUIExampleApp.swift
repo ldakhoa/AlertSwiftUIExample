05b9cb2eae0f2691bd448a15d0b9f241fd64c3f3
ledoan.khoa <ledoan.khoa@okg.com>
2024-04-02 10:04:27 +0800

    Initial Commit

---
+++ AlertSwiftUIExample/AlertSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIExampleApp.swift
+//  AlertSwiftUIExample
+//
+//  Created by Khoa Le on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
